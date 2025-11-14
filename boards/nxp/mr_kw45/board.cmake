a25df3638e4ea207f6c1e7342a4cb083de96f7f6
Mayank Mahajan <mayankmahajan.x@nxp.com>
2025-11-14 16:53:53 +0530

    Getting file not found errors but building more now.
    Maybe fundamental problems while creating soc in zephyr.
    
    Signed-off-by: Mayank Mahajan <mayankmahajan.x@nxp.com>

--- boards/nxp/mr_kw45/board.cmake
+++ boards/nxp/mr_kw45/board.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # board_runner_args(linkserver "--device=MCXW716CxxxA:FRDM-MCXW71")
-board_runner_args(jlink "--device=mcxw716" "--reset-after-load")
+board_runner_args(jlink "--device=kw45b41z83" "--reset-after-load")
 
 # include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
