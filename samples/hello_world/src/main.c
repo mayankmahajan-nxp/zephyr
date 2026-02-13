f9438a412dd3ee4af70265bff91ddf14d52f6684
Mayank Mahajan <mayankmahajan.x@nxp.com>
2026-02-13 13:24:32 +0530

    Zephyr MCXN947 demo driven by FRDM IMX93
    Signed-off-by: Mayank Mahajan <mayankmahajan.x@nxp.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,17 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	int i = 0;
+	while (true) {
+		k_sleep(K_MSEC(1000));
+		printf("printing hello on console %d\n", i++);
+	}
 
 	return 0;
 }
